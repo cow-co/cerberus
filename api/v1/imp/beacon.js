bed6fcd0bad041ea13f2b9462b81f540b2e5747a
cow <cow-co@users.noreply.github.com>
2021-11-06 21:48:07 +0000

    Start to Flesh Out Beacon Endpoint

--- api/v1/imp/beacon.js
+++ api/v1/imp/beacon.js
@@ -2,13 +2,33 @@
 const express = require("express");
 const router = express.Router();
 
+// TODO We'll improve this, by making the password based in some way on a key built in to the implant, and on the implant ID
+const checkPassword = (givenPassword) => {
+    let valid = false;
+    // TODO check if password-protection is enabled, in config
+    if(givenPassword === "password1") {
+        valid = true;
+    }
+    return valid;
+}
+
 router.post("/", async (req, res) => {
     const beaconContents = req.body;
     let responseStatus = 400;
-    if(beaconContents.id === 101) {
-        responseStatus = 200;
+    let responseMessage = "Bad Request";
+
+    // TODO Connect to database to check if ID already exists; if not, create an entry for it
+    if(!checkPassword(beaconContents.password)) {
+        responseStatus = 403;
+        responseMessage = "Unauthorised";
+    } else {
+        if(beaconContents.id === 101) {
+            responseStatus = 200;
+            responseMessage = "Success";
+        }
     }
-    return res.status(responseStatus).json({message: "Success"});
+    
+    return res.status(responseStatus).json({message: responseMessage});
 });
 
 module.exports = router;
--- spec/api/v1/imp/beacon.spec.js
+++ spec/api/v1/imp/beacon.spec.js
@@ -4,11 +4,19 @@ const server = require("../../../../app");
 
 describe("Beacon endpoint tests", () => {
     it("should respend with 200 to a properly-formatted beacon", async () => {
-        const beaconRes = await request(server).post("/api/v1/imp/beacon").send({id: 101});
+        const beaconRes = await request(server).post("/api/v1/imp/beacon").send({id: 101, password: "password1"});
         expect(beaconRes.status).to.equal(200);
     });
     it("should return 400 for an improperly-formatted request", async () => {
-        const beaconRes = await request(server).post("/api/v1/imp/beacon").send({id: 102});
+        const beaconRes = await request(server).post("/api/v1/imp/beacon").send({id: 102, password: "password1"});
         expect(beaconRes.status).to.equal(400);
+    });
+    it("should return 403 for an incorrect password", async () => {
+        const beaconRes = await request(server).post("/api/v1/imp/beacon").send({id: 102, password: "password2"});
+        expect(beaconRes.status).to.equal(403);
+    });
+    it("should return 403 for a missing password", async () => {
+        const beaconRes = await request(server).post("/api/v1/imp/beacon").send({id: 102});
+        expect(beaconRes.status).to.equal(403);
     });
 });
