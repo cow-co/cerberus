cd803d806923601a981b537c6c5ed9ae9ae02d0a
cow <cow-co@users.noreply.github.com>
2023-06-02 19:30:09 +0100

    Theming

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
 import "./App.css";
+import CssBaseline from "@mui/material/CssBaseline";
 import MainPage from "./components/pages/MainPage";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
-  return <MainPage />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme={true}>
+        <MainPage />
+      </CssBaseline>
+    </ThemeProvider>
+  );
 }
 
 export default App;
