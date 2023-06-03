2c569ea8e1691925e75fa8a5979e772b5b27e2d4
cow <cow-co@users.noreply.github.com>
2023-06-03 12:28:44 +0100

    Simple List of Implants

--- frontend/src/components/pages/ImplantsPane.jsx
+++ frontend/src/components/pages/ImplantsPane.jsx
@@ -1,20 +1,39 @@
 import { List, ListItem } from '@mui/material';
 import Container from '@mui/material/Container';
 
+// TODO Layout the implant list items better
+// TODO Colour the items according to if they are active or not
+// TODO Make the list items into buttons, which populate the task list (placeholder for now)
+// TODO "Show Inactive" radio button
+// TODO Actually make the query the backend
+// TODO "Refresh" button
+
+const implants = [
+  {
+    implantId: "1",
+    ip: "192.168.0.1",
+    os: "Linux",
+    beaconIntervalSeconds: 300,
+    isActive: true
+  }
+]
+
 function ImplantsPane() {
+  console.log(implants)
+  const implantsItems = implants.map(implant => {
+    return (
+      <ListItem className='listElement' key={implant.implantId}>
+        <h4>{implant.ip}</h4>
+        <h4>{implant.os}</h4>
+        <h4>{implant.beaconIntervalSeconds}</h4>
+      </ListItem>
+    )
+  })
   return (
     <Container fixed>
       <h2>Implants</h2>
       <List>
-        <ListItem className='listElement'>
-          <h4>Implant 1</h4>
-        </ListItem>
-        <ListItem className='listElement'>
-          <h4>Implant 2</h4>
-        </ListItem>
-        <ListItem className='listElement'>
-          <h4>Implant 3</h4>
-        </ListItem>
+        {implantsItems}
       </List>
     </Container>
   )
