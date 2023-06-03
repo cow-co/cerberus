f3e094d03008c9e5303f86bf80bdfdb551360cca
cow <cow-co@users.noreply.github.com>
2023-06-03 19:00:21 +0100

    Add Show-Inactive Functionality

--- frontend/src/components/pages/ImplantsPane.jsx
+++ frontend/src/components/pages/ImplantsPane.jsx
@@ -1,10 +1,9 @@
-import { List, ListItem } from '@mui/material';
+import { Checkbox, FormControlLabel, List, ListItem } from '@mui/material';
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import { useState } from 'react';
 
-// TODO Layout the implant list items better
-// TODO Colour the items according to if they are active or not
 // TODO Make the list items into buttons, which populate the task list (placeholder for now)
-// TODO "Show Inactive" radio button
 // TODO Actually make the query the backend
 // TODO "Refresh" button
 
@@ -15,23 +14,56 @@ const implants = [
     os: "Linux",
     beaconIntervalSeconds: 300,
     isActive: true
+  },
+  {
+    implantId: "2",
+    ip: "192.168.0.2",
+    os: "Windows",
+    beaconIntervalSeconds: 300,
+    isActive: true
+  },
+  {
+    implantId: "3",
+    ip: "8.8.8.8",
+    os: "BSD",
+    beaconIntervalSeconds: 180,
+    isActive: false
   }
-]
+];
+
 
 function ImplantsPane() {
-  console.log(implants)
-  const implantsItems = implants.map(implant => {
+  const [showInactive, setShowInactive] = useState(false);
+
+  function handleToggle() {
+    setShowInactive(!showInactive)
+  }
+
+  console.log("Rendering")
+  let filtered = implants
+  if(!showInactive) {
+    filtered = implants.filter(implant => implant.isActive)
+  }
+  const implantsItems = filtered.map(implant => {
+    const implantClass = (implant.isActive ? "implant active" : "implant inactive")
     return (
-      <ListItem className='listElement' key={implant.implantId}>
-        <h4>{implant.ip}</h4>
-        <h4>{implant.os}</h4>
-        <h4>{implant.beaconIntervalSeconds}</h4>
+      <ListItem className={`listElement ${implantClass}`} key={implant.implantId}>
+        <Grid item xs={4}>
+          <h4>IP: {implant.ip}</h4>
+        </Grid>
+        <Grid item xs={4}>
+          <h4>OS: {implant.os}</h4>
+        </Grid>
+        <Grid item xs={4}>
+          <h4>Beacon Interval: {implant.beaconIntervalSeconds} seconds</h4>
+        </Grid>
       </ListItem>
     )
   })
   return (
     <Container fixed>
       <h2>Implants</h2>
+      <FormControlLabel control={<Checkbox checked={showInactive} onClick={handleToggle}/>} label="Show Inactive" />
       <List>
         {implantsItems}
       </List>
