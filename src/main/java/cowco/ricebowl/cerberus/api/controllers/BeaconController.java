8a6a88c03a3ad42680a1a0986d8201734951db1e
cow <cow-co@users.noreply.github.com>
2023-06-04 19:45:42 +0100

    Add CORS to Allow Localhost for the Frontend

--- src/main/java/cowco/ricebowl/cerberus/api/controllers/BeaconController.java
+++ src/main/java/cowco/ricebowl/cerberus/api/controllers/BeaconController.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class BeaconController {
     @Autowired
     private ImplantsService activeImplantService;
 
+    @CrossOrigin
     @PostMapping(path = "/api/beacon", consumes = "application/json", produces = "application/json")
     public ResponseEntity<TasksListDTO> beacon(@RequestBody BeaconDTO beaconDto) {
         LOGGER.debug("Received beacon " + beaconDto.toString());
--- src/main/java/cowco/ricebowl/cerberus/api/controllers/ImplantsController.java
+++ src/main/java/cowco/ricebowl/cerberus/api/controllers/ImplantsController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import cowco.ricebowl.cerberus.api.representation.ImplantDTO;
 import cowco.ricebowl.cerberus.api.services.ImplantsService;
 
+@CrossOrigin(origins = { "http://localhost:3000", "http://192.168.0.1:3000", "https://localhost:3000",
+        "https://192.168.0.1:3000" }, maxAge = 3600)
 @RestController
 public class ImplantsController {
     @Autowired
