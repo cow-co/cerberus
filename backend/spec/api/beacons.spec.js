c7125f3641adc078776badc1a44f1eed2f8d3d3b
cow <cow-co@users.noreply.github.com>
2023-09-06 20:50:53 +0100

    Basic Success-Test

--- backend/spec/api/beacons.spec.js
+++ backend/spec/api/beacons.spec.js
@@ -4,6 +4,13 @@ const expect = require("chai").expect;
 
 describe("Beacon API tests", () => {
   it("should succeed", async () => {
-    await request(server).post();
+    const res = await request(server).post("/api/beacon/").send({
+      id: "eb706e60-5b2c-47f5-bc32-45e1765f7ce8",
+      ip: "192.168.0.1",
+      os: "Windows 6.1.7601.17592",
+      beaconIntervalSeconds: 300,
+    });
+
+    expect(res.statusCode).to.equal(200);
   });
 });
