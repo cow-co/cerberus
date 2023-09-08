032a4a128e867f61f3d8501407801f2231095b9d
cow <cow-co@users.noreply.github.com>
2023-09-08 12:23:43 +0100

    Remove Need for In-Mem Mongo

--- backend/spec/api/implants.spec.js
+++ backend/spec/api/implants.spec.js
@@ -1,16 +1,14 @@
 const request = require("supertest");
 const server = require("../../index");
 const expect = require("chai").expect;
-const utils = require("../utils");
+const sinon = require("sinon");
+const Implant = require("../../db/models/Implant");
 
 describe("Implant API Tests", () => {
-  beforeAll(async () => await utils.connect());
-
-  afterEach(async () => await utils.clearDB());
-
-  afterAll(async () => await utils.closeDB());
-
   it("should get all implants (empty array)", async () => {
+    let spied = sinon.stub(Implant, "find").callsFake(async () => {
+      return [];
+    });
     const res = await request(server).get("/api/implants/");
     expect(res.statusCode).to.equal(200);
     expect(res.body.implants.length).to.equal(0);
