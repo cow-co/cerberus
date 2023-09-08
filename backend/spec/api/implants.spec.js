ad762d0a4bf13f6c0d8dab9002a3aad7b31efb55
cow <cow-co@users.noreply.github.com>
2023-09-08 12:35:54 +0100

    Add Another Test

--- backend/spec/api/implants.spec.js
+++ backend/spec/api/implants.spec.js
@@ -5,6 +5,9 @@ const sinon = require("sinon");
 const Implant = require("../../db/models/Implant");
 
 describe("Implant API Tests", () => {
+  beforeEach(() => {
+    sinon.restore();
+  });
   it("should get all implants (empty array)", async () => {
     let spied = sinon.stub(Implant, "find").callsFake(async () => {
       return [];
@@ -12,5 +15,24 @@ describe("Implant API Tests", () => {
     const res = await request(server).get("/api/implants/");
     expect(res.statusCode).to.equal(200);
     expect(res.body.implants.length).to.equal(0);
+  });
+
+  it("should get all implants (non-empty array)", async () => {
+    let spied = sinon.stub(Implant, "find").callsFake(async () => {
+      return [
+        {
+          _id: "some-mongo-id",
+          id: "some-uuid",
+          ip: "192.168.0.1",
+          os: "Windows",
+          beaconIntervalSeconds: 300,
+          lastCheckinTimeSeconds: 0,
+          isActive: true,
+        },
+      ];
+    });
+    const res = await request(server).get("/api/implants/");
+    expect(res.statusCode).to.equal(200);
+    expect(res.body.implants.length).to.equal(1);
   });
 });
