b0ba50af37f52accb547b34a534fdba1e3f7413e
cow <cow-co@users.noreply.github.com>
2023-09-08 15:11:49 +0100

    Implement the Path Parameter

--- backend/spec/api/implants.spec.js
+++ backend/spec/api/implants.spec.js
@@ -7,18 +7,30 @@ const Implant = require("../../db/models/Implant");
 describe("Implant API Tests", () => {
   beforeEach(() => {
     sinon.restore();
-  });
-  it("should get all implants (empty array)", async () => {
-    let spied = sinon.stub(Implant, "find").callsFake(async () => {
-      return [];
+    const findStub = sinon.stub(Implant, "find").callsFake(async () => {
+      return [
+        {
+          _id: "some-mongo-id",
+          id: "some-uuid",
+          ip: "192.168.0.1",
+          os: "Windows",
+          beaconIntervalSeconds: 300,
+          lastCheckinTimeSeconds: 0,
+          isActive: true,
+        },
+        {
+          _id: "some-mongo-id",
+          id: "some-uuid",
+          ip: "192.168.0.1",
+          os: "Windows",
+          beaconIntervalSeconds: 300,
+          lastCheckinTimeSeconds: 0,
+          isActive: false,
+        },
+      ];
     });
-    const res = await request(server).get("/api/implants/");
-    expect(res.statusCode).to.equal(200);
-    expect(res.body.implants.length).to.equal(0);
-  });
 
-  it("should get all implants (non-empty array)", async () => {
-    let spied = sinon.stub(Implant, "find").callsFake(async () => {
+    findStub.withArgs({ isActive: true }).callsFake(async () => {
       return [
         {
           _id: "some-mongo-id",
@@ -31,7 +43,34 @@ describe("Implant API Tests", () => {
         },
       ];
     });
-    const res = await request(server).get("/api/implants/");
+  });
+  it("should get all implants (empty array)", async () => {
+    sinon.restore();
+    sinon.stub(Implant, "find").callsFake(async () => {
+      return [];
+    });
+    const res = await request(server).get("/api/implants");
+    expect(res.statusCode).to.equal(200);
+    expect(res.body.implants.length).to.equal(0);
+  });
+
+  it("should get all implants (non-empty array)", async () => {
+    const res = await request(server).get("/api/implants");
+    expect(res.statusCode).to.equal(200);
+    expect(res.body.implants.length).to.equal(1);
+  });
+
+  // TODO Tests for the includeInactive flag
+  it("should get all implants (including inactive)", async () => {
+    const res = await request(server).get("/api/implants?includeInactive=true");
+    expect(res.statusCode).to.equal(200);
+    expect(res.body.implants.length).to.equal(2);
+  });
+
+  it("should get all implants (explicitly excluding inactive)", async () => {
+    const res = await request(server).get(
+      "/api/implants?includeInactive=false"
+    );
     expect(res.statusCode).to.equal(200);
     expect(res.body.implants.length).to.equal(1);
   });
