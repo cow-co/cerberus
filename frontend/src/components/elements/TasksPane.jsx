f03efeb5d5dbec3fccc4c68cb63a79d96f8961c2
cow <cow-co@users.noreply.github.com>
2023-09-10 18:49:20 +0100

    Fix

--- frontend/src/components/elements/TasksPane.jsx
+++ frontend/src/components/elements/TasksPane.jsx
@@ -4,29 +4,29 @@ import Container from '@mui/material/Container';
 import { useEffect, useState } from 'react';
 import TaskItem from './TaskItem';
 
+// TODO Perhaps move all the backend-interaction code to its own file?
 function TasksPane({selectedImplant}) {
   const [showSent, setShowSent] = useState(false);
   const [tasks, setTasks] = useState([]);
   console.log("Rendering with implant: " + JSON.stringify(selectedImplant))
-
-  const refresh = async () => {
-    // TODO try/catch and error handling
-    // TODO Make the backend URL configurable#
-    const response = await fetch("http://localhost:5000/api/implants?includeSent=true")
-    const json = await response.json()
-    if (showSent) {
-      setTasks(json.tasks)
-    } else {
-      setTasks(json.tasks.filter(task => task.sent === false))
-    }
-  }
 
   const handleToggle = () => {
     setShowSent(!showSent)
   }
 
   useEffect(() => {
-    refresh()
+    async function fetchTasks() {
+      // TODO try/catch and error handling
+      // TODO Make the backend URL configurable#
+      const response = await fetch("http://localhost:5000/api/implants?includeSent=true")
+      const json = await response.json()
+      if (showSent) {
+        setTasks(json.tasks)
+      } else {
+        setTasks(json.tasks.filter(task => task.sent === false))
+      }
+    }
+    fetchTasks()
   }, [selectedImplant])
 
   const tasksItems = tasks.map(task => {
@@ -36,7 +36,6 @@ function TasksPane({selectedImplant}) {
   return (
     <Container fixed>
       <h2>Tasks for {selectedImplant.id}</h2>
-      <Button variant='contained' onClick={refresh}>Refresh</Button>
       <FormControlLabel control={<Checkbox checked={showSent} onClick={handleToggle}/>} label="Show Sent" />
       <List>
         {tasksItems}
