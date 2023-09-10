6f22544193c61d60eef5c567e7eb1b159b71b2e1
cow <cow-co@users.noreply.github.com>
2023-09-10 14:18:36 +0100

    Update Backend to Serve Built Files

--- backend/index.js
+++ backend/index.js
@@ -8,20 +8,27 @@ const logger = require("./utils/logger");
 const YAML = require("yamljs");
 const swaggerDocBeaconing = YAML.load("openapi/beaconing.yaml");
 const swaggerDocImplants = YAML.load("openapi/implants.yaml");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// if (process.env.NODE_ENV === "production") {
-const db = require("./config/dbConfig").mongo_uri;
-mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() =>
-    logger.log("index.js", "MongoDB connection successful", logger.levels.INFO)
-  )
-  .catch((err) => logger.log("index.js", err, logger.levels.ERROR));
-// }
+app.use(express.static(path.join(__dirname, "build")));
+
+if (process.env.NODE_ENV === "production") {
+  const db = require("./config/dbConfig").mongo_uri;
+  mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() =>
+      logger.log(
+        "index.js",
+        "MongoDB connection successful",
+        logger.levels.INFO
+      )
+    )
+    .catch((err) => logger.log("index.js", err, logger.levels.ERROR));
+}
 
 app.use(
   "/api-docs/beaconing",
@@ -46,14 +53,26 @@ let server = app.listen(port, async () => {
 const stop = () => {
   logger.log("index.js", "Closing server...", logger.levels.INFO);
 
-  // if (process.env.NODE_ENV === "production") {
-  mongoose.disconnect();
-  // }
+  if (process.env.NODE_ENV === "production") {
+    mongoose.disconnect();
+  }
 
   server.shutdown(() => {
     logger.log("index.js", "Server closed...", logger.levels.INFO);
   });
 };
+
+const serveProdClient = () => {
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get(/^\/(?!api).*/, (req, res) => {
+      res.sendFile(path.join(__dirname, "./build/index.html"));
+    });
+
+    console.log("Serving React App...");
+  }
+};
+serveProdClient();
 
 module.exports = server;
 module.exports.stop = stop;
