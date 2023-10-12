cdfc6d9efd6c90f24af8348473a1b60806ca8602
cow <cow-co@users.noreply.github.com>
2023-10-12 23:12:51 +0100

    Exception Handling Around API Endpoints

--- backend/api/users.js
+++ backend/api/users.js
@@ -8,6 +8,7 @@ const {
   findUserByName,
   findUserById,
 } = require("../security/user-and-access-manager");
+const { log, levels } = require("../utils/logger");
 
 router.get("/user/:username", verifySession, async (req, res) => {
   let status = statusCodes.OK;
@@ -17,15 +18,22 @@ router.get("/user/:username", verifySession, async (req, res) => {
   };
 
   const chosenUser = req.params.username.trim();
-  const result = await findUserByName(chosenUser);
-  if (result.user) {
-    response.user = {
-      id: result.user.id,
-      name: result.user.name,
-    };
-  } else {
-    status = statusCodes.BAD_REQUEST;
-    response.errors.push("User not found");
+
+  try {
+    const result = await findUserByName(chosenUser);
+    if (result.user) {
+      response.user = {
+        id: result.user.id,
+        name: result.user.name,
+      };
+    } else {
+      status = statusCodes.BAD_REQUEST;
+      response.errors.push("User not found");
+    }
+  } catch (err) {
+    log("GET /user/:username", err, levels.ERROR);
+    status = statusCodes.INTERNAL_SERVER_ERROR;
+    response.errors = ["Internal Server Error"];
   }
 
   res.status(status).json(response);
@@ -36,19 +44,26 @@ router.delete("/user/:userId", verifySession, checkAdmin, async (req, res) => {
   let response = {
     errors: [],
   };
-
   const chosenUser = req.params.userId.trim();
-  const result = await findUserById(chosenUser);
-  if (!result.user) {
-    response.errors = result.errors;
-    status = statusCodes.BAD_REQUEST;
-  } else {
-    const errors = await removeUser(chosenUser);
-    response.errors = errors;
-    if (errors.length > 0) {
-      status = statusCodes.INTERNAL_SERVER_ERROR;
+
+  try {
+    const result = await findUserById(chosenUser);
+    if (!result.user) {
+      response.errors = result.errors;
+      status = statusCodes.BAD_REQUEST;
+    } else {
+      const errors = await removeUser(chosenUser);
+      response.errors = errors;
+      if (errors.length > 0) {
+        status = statusCodes.INTERNAL_SERVER_ERROR;
+      }
     }
+  } catch (err) {
+    log("DELETE /user/:userId", err, levels.ERROR);
+    status = statusCodes.INTERNAL_SERVER_ERROR;
+    response.errors = ["Internal Server Error"];
   }
+
   res.status(status).json(response);
 });
 
